9473d633d346db11de8f443922285355d0732378
Tanmay s ail <142146589+HellTanmay@users.noreply.github.com>
2025-08-18 20:54:52 +0530

    addpost link created

--- Client/src/Posts/AddPost.jsx
+++ Client/src/Posts/AddPost.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AddPost = () => {
   return (
     <div>
-      
+      <h1>Hello</h1>
     </div>
   )
 }
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,13 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import AddPost from "./Posts/AddPost.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />}/>
-      
+      <Route path="/addPost" element={<AddPost />}/>
     </Routes>
     </BrowserRouter>
   </StrictMode>
